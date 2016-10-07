94b26ebef25577e8bbb650f755abccdb0e2dd5cb
Daniel Jones <tortxof@gmail.com>
2016-10-06 22:53:20 -0400

    Added Home component for index route.

--- client/main.js
+++ client/main.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router, Route, Link, IndexRedirect, browserHistory} from 'react-router'
+import {Router, Route, Link, IndexRoute, browserHistory} from 'react-router'
 
 import App from './app'
+import Home from './home'
 import ApiKeys from './api_keys'
 import Collections from './collections'
 import Images from './images'
@@ -13,6 +14,7 @@ ReactDOM.render(
   (
     <Router history={browserHistory}>
       <Route path='/' component={App}>
+        <IndexRoute component={Home} />
         <Route path='api-keys' component={ApiKeys} />
         <Route path='collections' component={Collections} />
         <Route path='images' component={Images} />
