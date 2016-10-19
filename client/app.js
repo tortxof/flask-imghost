0ea9169201ba296cad1343d950f7d6c1dd262780
Daniel Jones <tortxof@gmail.com>
2016-10-18 22:27:26 -0400

    Can remove images from collection.

--- client/app.js
+++ client/app.js
@@ -51,6 +51,19 @@ export default React.createClass({
       })
     })
   },
+  removeImagesFromCollection(images, collection) {
+    return images.map(image => {
+      const uri = `/api/collections/${collection}/images/${image.s3_key}`
+      return fetch(uri, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        credentials: 'same-origin',
+        method: 'DELETE'
+      })
+    })
+  },
   handleUpload(file) {
     fetch('/api/signed-post', {credentials: 'same-origin'})
     .then(response => {
@@ -261,6 +274,7 @@ export default React.createClass({
                 handleUpload: this.handleUpload,
                 toggleImageSelect: this.toggleImageSelect,
                 addImagesToCollection: this.addImagesToCollection,
+                removeImagesFromCollection: this.removeImagesFromCollection,
                 updateApiKeys: this.updateApiKeys,
                 updateCollections: this.updateCollections,
                 updateImages: this.updateImages,
--- client/collection_detail.js
+++ client/collection_detail.js
@@ -7,6 +7,9 @@ export default React.createClass({
     return {collection: {images: []}}
   },
   componentDidMount() {
+    this.updateImages()
+  },
+  updateImages() {
     const uri = `/api/collections/${this.props.params.name}`
     fetch(uri, {
       headers: {
@@ -18,6 +21,9 @@ export default React.createClass({
     .then(response => {
       if (response.status === 200) {
         response.json().then(collection => {
+          collection.images = collection.images.map(image => {
+            return Object.assign({}, image, {selected: false})
+          })
           this.setState({
             collection: collection
           })
@@ -25,16 +31,36 @@ export default React.createClass({
       }
     })
   },
+  toggleImageSelect(e) {
+    const key = e.target.dataset.key
+    const images = this.state.collection.images.map(image => {
+      if (image.s3_key === key) {
+        image.selected = !image.selected
+      }
+      return image
+    })
+    this.setState({
+      collection: Object.assign({}, this.state.collection, {images: images})
+    })
+  },
+  handleRemove() {
+    const images = this.state.collection.images.filter(image => image.selected)
+    const collection = this.state.collection.name
+    Promise.all(this.props.removeImagesFromCollection(images, collection))
+    .then(responses => {this.updateImages()})
+  },
   render() {
     const images = this.state.collection.images.map(image => (
       <Image
         key={image.s3_key}
         image={image}
+        handleClick={this.toggleImageSelect}
       />
     ))
     return (
       <div className='collection-detail'>
         <div>Collection: {this.props.params.name}</div>
+        <button onClick={this.handleRemove}>Remove from collection</button>
         <div className='images'>
           {images}
         </div>
