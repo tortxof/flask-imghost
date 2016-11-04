0ed7544e646a665cc210be5928c3ce9cf0975dfe
Daniel Jones <tortxof@gmail.com>
2016-11-04 13:46:03 -0400

    Changed migrate.py to import from sqlite to postgres.

--- migrate.py
+++ migrate.py
@@ -1,26 +1,44 @@
-from playhouse.migrate import migrate, SqliteMigrator
-from playhouse.reflection import Introspector
+from playhouse.shortcuts import model_to_dict
 
+import sqlite_models
 import models
 
-migrator = SqliteMigrator(models.database)
-introspector = Introspector.from_database(models.database)
+sqlite_models.database.connect()
+models.database.connect()
 
-generated_models = introspector.generate_models()
+old_users = sqlite_models.User.select()
 
-if 'image' in generated_models:
-    if 'colors' not in generated_models['image']._meta.fields.keys():
-        with models.database.transaction():
-            migrate(
-                migrator.add_column('image', 'colors', models.Image.colors)
-            )
-    if 'size' not in generated_models['image']._meta.fields.keys():
-        with models.database.transaction():
-            migrate(
-                migrator.add_column('image', 'size', models.Image.size)
-            )
-    if 'thumbs' not in generated_models['image']._meta.fields.keys():
-        with models.database.transaction():
-            migrate(
-                migrator.add_column('image', 'thumbs', models.Image.thumbs)
-            )
+for old_user in old_users:
+    new_user = models.User.create(**model_to_dict(old_user, recurse=False))
+
+    old_apikeys = sqlite_models.ApiKey.select().where(sqlite_models.ApiKey.user == old_user)
+    for old_apikey in old_apikeys:
+        models.ApiKey.create(**model_to_dict(old_apikey, recurse=False))
+
+    old_images = sqlite_models.Image.select().where(sqlite_models.Image.user == old_user)
+    for old_image in old_images:
+        try:
+            models.Image.create(**model_to_dict(old_image, recurse=False))
+        except:
+            print(model_to_dict(old_image, recurse=False))
+
+    old_collections = sqlite_models.Collection.select().where(sqlite_models.Collection.user == old_user)
+    for old_collection in old_collections:
+        models.Collection.create(**model_to_dict(old_collection, recurse=False))
+        old_imagecollections = sqlite_models.ImageCollection.select().where(sqlite_models.ImageCollection.collection == old_collection)
+        for old_imagecollecton in old_imagecollections:
+            models.ImageCollection.create(**model_to_dict(old_imagecollecton, recurse=False))
+
+tables = [
+    'user',
+    'apikey',
+    'image',
+    'collection',
+    'imagecollection',
+]
+
+for table in tables:
+    models.database.execute_sql('SELECT setval(pg_get_serial_sequence(\'{table}\', \'id\'), coalesce(max(id),0) + 1, false) FROM "{table}";'.format(table=table))
+
+sqlite_models.database.close()
+models.database.close()
---
+++ sqlite_models.py
@@ -0,0 +1,59 @@
+from peewee import (
+    SqliteDatabase, Model, ForeignKeyField, CharField, TextField,
+    DateTimeField, IntegrityError
+    )
+
+from playhouse.sqlite_ext import PrimaryKeyAutoIncrementField
+
+database = SqliteDatabase('/data/data.db')
+
+class BaseModel(Model):
+    id = PrimaryKeyAutoIncrementField()
+    class Meta():
+        database = database
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    password = CharField()
+    email = CharField(unique=True)
+    date_created = DateTimeField()
+
+class Collection(BaseModel):
+    user = ForeignKeyField(User)
+    name = CharField(unique=True)
+
+    def images(self):
+        return (
+            Image.select()
+            .join(ImageCollection)
+            .where(ImageCollection.collection == self)
+        )
+
+class Image(BaseModel):
+    user = ForeignKeyField(User)
+    s3_key = CharField(unique=True)
+    s3_bucket = CharField()
+    title = CharField(default='')
+    description = TextField(default='')
+    colors = CharField(default='')
+    size = CharField(default='')
+    thumbs = CharField(default='')
+    date_created = DateTimeField()
+
+    class Meta:
+        order_by = ('-date_created',)
+
+class ImageCollection(BaseModel):
+    image = ForeignKeyField(Image)
+    collection = ForeignKeyField(Collection)
+
+    class Meta:
+        indexes = (
+            (('image', 'collection'), True),
+        )
+
+class ApiKey(BaseModel):
+    key = CharField(unique=True)
+    user = ForeignKeyField(User)
+    description = CharField(default='')
+    date_created = DateTimeField()
